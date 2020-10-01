10788875097c6cfa649dad281757bbd3f88b4944
Vile <vilde70@gmail.com>
2020-10-01 13:19:36 +0300

    Testing eslint again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-    DevOps dev branch
+      DevOps dev branch
     (murmeli takeover 8D).
    </p>
         <a
